a7cd0297ea7d0539fa85d9f8e38055430890fe35
Sooriasri234 <sooriasri1964@gmail.com>
2026-03-29 19:09:39 +0530

    Added save to file feature

--- todo.py
+++ todo.py
@@ -4,6 +4,10 @@ def show_tasks():
     print("Tasks:")
     for i, task in enumerate(tasks):
         print(f"{i+1}. {task}")
+def save_tasks():
+    with open("tasks.txt", "w") as f:
+        for task in tasks:
+            f.write(task + "\n")
 
 if __name__ == "__main__":
     tasks.append("Learn Git")
