e18c5845991e62d3e6aae5aee772e07bc1263893
Sooriasri234 <sooriasri1964@gmail.com>
2026-03-29 19:08:38 +0530

    Added update task feature

--- todo.py
+++ todo.py
@@ -4,6 +4,9 @@ def show_tasks():
     print("Tasks:")
     for i, task in enumerate(tasks):
         print(f"{i+1}. {task}")
+def update_task(index, new_task):
+    if 0 <= index < len(tasks):
+        tasks[index] = new_task
 
 if __name__ == "__main__":
     tasks.append("Learn Git")
