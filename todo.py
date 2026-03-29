10e90b26d9f28ea39f19c49de315959ae943063c
Sooriasri234 <sooriasri1964@gmail.com>
2026-03-29 19:04:30 +0530

    Added add task feature

--- todo.py
+++ todo.py
@@ -4,6 +4,8 @@ def show_tasks():
     print("Tasks:")
     for i, task in enumerate(tasks):
         print(f"{i+1}. {task}")
+def add_task(task):
+    tasks.append(task)
 
 if __name__ == "__main__":
     tasks.append("Learn Git")
